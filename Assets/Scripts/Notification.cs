8b1971e5513e94da6c7abb413cb20d1dcfbf4183
Gaspard BROUSSAUD <gaspard.broussaud1999@gmail.com>
2019-02-25 15:47:36 +0100

    NotificationSystem

---
+++ Assets/Scripts/Notification.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Assets.SimpleAndroidNotifications
+{
+    public class Notification : MonoBehaviour
+    {
+
+        public string notifTitre = "Titre de notif";
+        public string notifTexte = "Texte de la notification";
+
+        public void SendNotif()
+        {
+            NotificationManager.Send(TimeSpan.FromSeconds(5), notifTitre, notifTexte, Color.white);
+        }
+    }
+}
