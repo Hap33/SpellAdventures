2aabbb955d745ad802308ba58601d3e49f5410fc
Hap33 <36961317+Hap33@users.noreply.github.com>
2019-03-04 14:41:28 +0100

    Ajout Menu + Pause + Indications Spell

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+namespace Assets.SimpleAndroidNotifications
+{
+    public class MenuScript : MonoBehaviour
+    {
+
+        public string notifTitre = "Spell Adventure";
+        public string notifTexte = "Ne nous oubliez pas !";
+
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public void QuitGame()
+        {
+            NotificationManager.Send(TimeSpan.FromSeconds(20), notifTitre, notifTexte, Color.white);
+            Application.Quit();
+        }
+    }
+}
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
 }
